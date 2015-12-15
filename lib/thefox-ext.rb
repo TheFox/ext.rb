d788de1543500c82dbc64c36fec9d64ebcf54494
Christian Mayer <christian@fox21.at>
2015-12-15 10:28:17 +0100

    Integer extension added. #RN

--- lib/thefox-ext.rb
+++ lib/thefox-ext.rb
@@ -2,6 +2,7 @@
 require 'thefox-ext/version'
 
 require 'thefox-ext/ext/false'
+require 'thefox-ext/ext/integer'
 require 'thefox-ext/ext/nil'
 require 'thefox-ext/ext/string'
 require 'thefox-ext/ext/true'
--- lib/thefox-ext/version.rb
+++ lib/thefox-ext/version.rb
@@ -1,7 +1,7 @@
 
 module TheFox
 	module Ext
-		VERSION = '1.1.0-dev.7'
+		VERSION = '1.1.0-dev.8'
 		DATE = '2015-12-07'
 	end
 end
