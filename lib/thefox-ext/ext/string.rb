ff062423f2939297001d869fbc97523bf231f2b6
Christian Mayer <christian@fox21.at>
2015-12-15 10:40:31 +0100

    String: to_i32a function added. #RN
    Convert a String to a Integer 32-bit Array.

--- lib/thefox-ext/ext/string.rb
+++ lib/thefox-ext/ext/string.rb
@@ -16,6 +16,21 @@ class String
 			.join(' ')
 	end
 	
+	def to_i32a
+		len = self.length
+		len_w = (len >> 2) + (len & 0x3).to_b.to_i
+		
+		out = (0..(len_w - 1)).map{ |n| [n, 0] }.to_h
+		
+		i = 0
+		self.split('').each do |s|
+			out[i >> 2] |= (s.ord << ((3 - (i & 0x3)) << 3))
+			i += 1
+		end
+		
+		out
+	end
+	
 	def to_utf8
 		if is_utf8?
 			self.force_encoding('UTF-8')
