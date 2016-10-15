41e60658e4fee3564dfc631e1928068d019440d2
Christian Mayer <christian@fox21.at>
2016-10-15 23:27:28 +0200

    GitLab Deployment added.
    Closes #2.

--- lib/thefox-ext/version.rb
+++ lib/thefox-ext/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Ext
 		RELEASE_ID = 11
-		VERSION = '1.5.0-dev.1'
-		DATE = '2016-09-12'
+		VERSION = '1.5.0-dev.2'
+		DATE = '2016-10-15'
 		HOMEPAGE = 'https://github.com/TheFox/ext.rb'
 	end
 end
