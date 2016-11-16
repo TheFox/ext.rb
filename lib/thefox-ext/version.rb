5ad9ecae4f1bdbbedc8ad95268ec3da934eec8c8
Christian Mayer <christian@fox21.at>
2016-11-16 20:12:08 +0100

    Ruby Common Big 2016-11-08

--- lib/thefox-ext/version.rb
+++ lib/thefox-ext/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module Ext
 		RELEASE_ID = 11
-		VERSION = '1.5.0-dev.2'
-		DATE = '2016-10-15'
+		VERSION = '1.5.0-dev.3'
+		DATE = '2016-11-16'
 		HOMEPAGE = 'https://github.com/TheFox/ext.rb'
 	end
 end
