9b5074ed13eb90c66adb562d11c92fda16bbe1e6
Christian Mayer <christian@fox21.at>
2021-04-05 16:19:42 +0200

    Dev Version 1.9.0

--- CHANGELOG-v1.md
+++ CHANGELOG-v1.md
@@ -1,5 +1,7 @@
 # Release Notes for Extended Ruby Classes v1.x
 
+## v1.9.0 [unreleased]
+
 ## v1.8.0
 
 - Ruby v3 Support
--- lib/thefox-ext/version.rb
+++ lib/thefox-ext/version.rb
@@ -1,7 +1,7 @@
 
 module TheFox
   module Ext
-    VERSION = '1.8.0'
+    VERSION = '1.9.0-dev.1'
     DATE = '2021-04-05'
     HOMEPAGE = 'https://github.com/TheFox/ext.rb'
   end
