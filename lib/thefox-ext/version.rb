83dcf86150684961cdeba9e27e360bdcd48e8c50
Christian Mayer <christian@fox21.at>
2021-04-19 10:14:28 +0200

    Version 2.0.0-rc.1.

--- lib/thefox-ext/version.rb
+++ lib/thefox-ext/version.rb
@@ -1,8 +1,8 @@
 
 module TheFox
   module Ext
-    VERSION = '2.0.0-dev.2'
-    DATE = '2021-04-09'
+    VERSION = '2.0.0-rc.1'
+    DATE = '2021-04-19'
     HOMEPAGE = 'https://github.com/TheFox/ext.rb'
   end
 end
